03c0dc8c65bd2b43fbab2abcc7786908f5007a8d
Niklas <niklas@kizo-agency.com>
2022-06-14 15:50:00 +0200

    Setup jest

--- nextjs_testing_app/base-concert-venue/jest.setup.js
+++ nextjs_testing_app/base-concert-venue/jest.setup.js
@@ -4,3 +4,16 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+// src/setupTests.js
+import { server } from "./__tests__/__mocks__/msw/server.js";
+
+// Establish API mocking before all tests
+beforeAll(() => server.listen());
+
+// reset any request handlers that we may add during the tests,
+// so they don't affect other tests
+afterEach(() => server.resetHandlers());
+
+// clean up after the tests are finsihed
+afterAll(() => server.close());
