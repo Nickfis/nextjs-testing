9ff012b928f4fd95045db757aeeb8fe14c3b1d1c
Niklas <niklas@kizo-agency.com>
2022-06-16 16:29:45 +0200

    Test static routes

--- nextjs_testing_app/base-concert-venue/cypress/integration/routes.test.js
+++ nextjs_testing_app/base-concert-venue/cypress/integration/routes.test.js
@@ -3,3 +3,11 @@ it("displays correct heading when navigating to shows route", () => {
   cy.findByRole("button", { name: /shows/i }).click();
   cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
 });
+
+it("Displays correct heading on bands page", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /bands/i }).click();
+  cy.findByRole("heading", { name: /our illustrious performers/i }).should(
+    "exist"
+  );
+});
