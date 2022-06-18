1880e8600767d87eb64696267a93a21f5af4911f
Niklas <niklas@kizo-agency.com>
2022-06-18 18:04:03 +0200

    Check route with route param

--- nextjs_testing_app/base-concert-venue/__tests__/api/shows.test.ts
+++ nextjs_testing_app/base-concert-venue/__tests__/api/shows.test.ts
@@ -1,6 +1,7 @@
 import { testApiHandler } from "next-test-api-route-handler";
 
 import showsHandler from "@/pages/api/shows/index";
+import showIdHandler from "@/pages/api/shows/[showId]";
 import { expect } from "@jest/globals";
 import { readFakeData } from "@/__tests__/__mocks__/fakeData";
 
@@ -14,6 +15,24 @@ test("/api/shows returns shows from database", async () => {
 
       const { fakeShows } = await readFakeData();
       expect(json).toEqual({ shows: fakeShows });
+    },
+  });
+});
+
+test("GET /api/shows/[showId] returns the data for the correct show ID", async () => {
+  await testApiHandler({
+    handler: showIdHandler,
+    paramsPatcher: (params) => {
+      params.showId = 0;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: "GET" });
+      expect(res.status).toBe(200);
+
+      const json = await res.json();
+      const { fakeShows } = await readFakeData();
+      const expectedShow = fakeShows.find((show) => show.id === 0);
+      expect(json).toEqual({ show: expectedShow });
     },
   });
 });
