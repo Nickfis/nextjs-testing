99188afbb8778896f1b3a4b317b113c41763b6bf
Niklas <niklas@kizo-agency.com>
2022-06-19 10:59:12 +0200

    Unauthorized test

--- nextjs_testing_app/base-concert-venue/__tests__/api/reservations.test.ts
+++ nextjs_testing_app/base-concert-venue/__tests__/api/reservations.test.ts
@@ -1,15 +1,17 @@
 import { testApiHandler } from "next-test-api-route-handler";
-import createReservationHandler from "@/pages/api/reservations/[reservationId]";
+import reservationHandler from "@/pages/api/reservations/[reservationId]";
 import userReservationsHandler from "@/pages/api/users/[userId]/reservations";
 import { expect } from "@jest/globals";
+import { validateToken } from "@/lib/auth/utils";
 
 jest.mock("@/lib/auth/utils");
+const mockValidateToken = validateToken as jest.Mock;
 
 // after making reservations, there should be 3 for user 1
 test("User can create reservation and it gets added to their reservations", async () => {
   // make reservations
   await testApiHandler({
-    handler: createReservationHandler,
+    handler: reservationHandler,
     paramsPatcher: (params) => {
       params.reservationId = 100;
     },
@@ -41,6 +43,32 @@ test("User can create reservation and it gets added to their reservations", asyn
       const json = await res.json();
       console.log(json);
       expect(json.userReservations).toHaveLength(3);
+    },
+  });
+});
+
+test("POST /api/reservations/[reservationId] returns 401 status when not authorized", async () => {
+  mockValidateToken.mockResolvedValue(false);
+
+  // make reservations
+  await testApiHandler({
+    handler: reservationHandler,
+    paramsPatcher: (params) => {
+      params.reservationId = 100;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          seatCount: 5,
+          userId: 1,
+          showId: 0,
+        }),
+      });
+      expect(res.status).toBe(401);
     },
   });
 });
